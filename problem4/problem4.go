7f3daa60c31b2737fa7b16091c6d80d227f30647
Luka <luka+git@geluti.org>
2019-01-18 21:31:57 +0100

    Problem 4: Optimized algorithm; removed Log10()
    In this particular case, we know that we should find a palindrome formed
    by two three-digits number, so we're looking for x as:
    
    100×100 ≤ x ≤ 999×999
    10000 ≤ x ≤ 998001
    
    So we're looking at a five or six digits number.
    Given this, we can afford a single sub-nanosecond comparison instead of
    a base-10 logarithm executed in nearly 20ns.
    
    The remaining of the algorithm was improved through an empirical method,
    benchmarking each modification in order to find the best way of caching
    values to registers, looping, testing conditions, …
    
    A parallelized algorithm was too expensive for synchronization due to
    mutexes locks and unlocks (reaching hundreds of microseconds)

--- problem4/problem4.go
+++ problem4/problem4.go
@@ -2,58 +2,44 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"sync"
 )
 
-func isPalindrome(n uint) bool {
-	switch uint(math.Log10(float64(n))) {
-	case 0:
+func is6Digits(n uint) bool {
+	if n >= 100000 {
 		return true
-	case 1:
-		return n%10 == n/10
-	case 2:
-		return n%10 == n/100
-	case 3:
-		return (n%10 == n/1000) &&
-			((n%100-n%10)/10 == n/100-((n/1000)*10))
-	case 4:
-		return (n%10 == n/10000) &&
-			((n%100-n%10)/10 == n/1000-((n/10000)*10))
-	case 5:
+	} else {
+		return false
+	}
+}
+
+func isPalindrome(n uint) bool {
+	if is6Digits(n) {
 		return (n%10 == n/100000) &&
 			((n%100-n%10)/10 == n/10000-((n/100000)*10)) &&
 			((n%1000-n%100)/100 == n/1000-((n/10000)*10))
 	}
-	return false
+	return (n%10 == n/10000) &&
+		((n%100-n%10)/10 == n/1000-((n/10000)*10))
 }
 
 func compute() (match uint) {
 	match = 100 * 100
 	const y uint = 999
 	var x uint = y
-	var matchX uint = 100
 
-	var wg sync.WaitGroup
-	for ; x >= matchX; x-- {
-		wg.Add(1)
-		go (func(x uint) {
-			defer wg.Done()
-			for i := uint(0); ; i++ {
-				yMinusI := y - i
-				num := x * yMinusI
-				if !(yMinusI > x && num > match) {
-					break
-				}
-
-				if isPalindrome(num) {
-					match = num
-					matchX = x
-				}
+	for ; x*y >= match; x-- {
+		for i := uint(0); ; i++ {
+			yMinusI := y - i
+			num := x * yMinusI
+			if !(yMinusI >= x && num > match) {
+				break
 			}
-		})(x)
+
+			if isPalindrome(num) {
+				match = num
+			}
+		}
 	}
-	wg.Wait()
 	return
 }
 
